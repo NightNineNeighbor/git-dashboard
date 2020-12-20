c2df566fe5d16650ddefc33aac7e16ab85a7522c
NaeuNyeoNg <dud9203@gmail.com>
2020-12-21 00:53:20 +0900

    project 초기 세팅

---
+++ src/main/kotlin/com/dnnn/git/dashboard/GitDashboardApplication.kt
@@ -0,0 +1,12 @@
+package com.dnnn.git.dashboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GitDashboardApplication
+
+fun main(args: Array<String>) {
+	runApplication<GitDashboardApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/dnnn/git/dashboard/GitDashboardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dnnn.git.dashboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GitDashboardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
